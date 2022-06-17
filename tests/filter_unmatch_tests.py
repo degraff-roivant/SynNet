64521966dabc09912b3d729d75377c9d89078b0b
david graff <david.graff@roivant.com>
2022-06-17 09:08:13 -0400

    merging

--- tests/filter_unmatch_tests.py
+++ tests/filter_unmatch_tests.py
@@ -7,9 +7,9 @@ from tqdm import tqdm
 from syn_net.utils.data_utils import *
 
 
-if __name__ == '__main__':
-    r_path = './data/ref/rxns_hb.json.gz'
-    bb_path = '/home/whgao/scGen/synth_net/data/enamine_us.csv.gz'
+if __name__ == "__main__":
+    r_path = "./data/ref/rxns_hb.json.gz"
+    bb_path = "/home/whgao/scGen/synth_net/data/enamine_us.csv.gz"
     r_set = ReactionSet()
     r_set.load(r_path)
     matched_mols = set()
@@ -17,10 +17,10 @@ if __name__ == '__main__':
         for a_list in r.available_reactants:
             matched_mols = matched_mols | set(a_list)
 
-    original_mols = pd.read_csv(bb_path, compression='gzip')['SMILES'].tolist()
+    original_mols = pd.read_csv(bb_path, compression="gzip")["SMILES"].tolist()
 
-    print('Total building blocks number:', len(original_mols))
-    print('Matched building blocks number:', len(matched_mols))
+    print("Total building blocks number:", len(original_mols))
+    print("Matched building blocks number:", len(matched_mols))
 
-    df = pd.DataFrame({'SMILES': list(matched_mols)})
-    df.to_csv('./data/building_blocks_matched.csv.gz', compression='gzip')
+    df = pd.DataFrame({"SMILES": list(matched_mols)})
+    df.to_csv("./data/building_blocks_matched.csv.gz", compression="gzip")
